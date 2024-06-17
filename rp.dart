2808cbd889dfadbf6758ef8c03642bcffe6dc879
Tuấn Anh <112914175+TuannAnhh2002@users.noreply.github.com>
2024-06-17 16:05:15 +0700

    Create rp.dart

---
+++ rp.dart
@@ -0,0 +1,14 @@
+
+Perform testing of new DUT firmware on several stations, confirm and support Min Le in writing reports, summarizing errors and LOGs.
+
+Training for new members on the testing process of some stations, test plans, functions of files in mutiltest.
+Handle security issues and back up data on the server and database.
+Reinstall the comport for the DONUT_SWDL station
+Retest the functions of the SMT application
+Perform conversion from EVT to DVT for test stations
+
+
+Add facial recognition function on server
+Add the function of automatically sending emails to employees for each export on 
+Encrypt the database folder, ensuring security
+Add automatic backup function on the server
